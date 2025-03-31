df3bcdc138a449a35403cc3590c1caececde648f
Petrik Diak <diak@petrik.hu>
2025-03-31 15:43:51 +0200

    Adatszerkezet, JS osztály definiálva

---
+++ vite-project/src/main.js
@@ -0,0 +1,57 @@
+
+class DataItem {
+  constructor(id, name, category) {
+    this.id = id;
+    this.name = name;
+    this.category = category;
+  }
+}
+
+class DataManager {
+  constructor() {
+    this.data = [];
+    this.nextId = 1;
+  }
+  addData(name, category) {
+    const newItem = new DataItem(this.nextId, name, category);
+    this.data.push(newItem);
+    this.nextId++;
+  }
+
+  getData() {
+    return this.data;
+  }
+
+  renderTable() {
+    const tableBody = document.querySelector('#data-table tbody');
+    tableBody.innerHTML = '';
+
+    this.data.forEach(item => {
+      const row = document.createElement('tr');
+      row.innerHTML = `
+        <td>${item.id}</td>
+        <td>${item.name}</td>
+        <td>${item.category}</td>
+      `;
+      tableBody.appendChild(row);
+    });
+  }
+}
+
+const dataManager = new DataManager();
+
+const form = document.querySelector('#add-item-form');
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  const name = document.querySelector('#name').value;
+  const category = document.querySelector('#category').value;
+
+  if (name && category) {
+    dataManager.addData(name, category);
+    dataManager.renderTable();
+    form.reset();
+  } else {
+    alert('Minden mezőt ki kell tölteni!');
+  }
+});
